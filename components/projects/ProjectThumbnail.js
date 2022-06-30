a0eac90b7629c43aa42c5a7bf91608c2dc839ddc
Risen-97 <m.fareeed1997@gmail.com>
2022-06-30 23:43:39 +0300

    lazy loading images

--- components/projects/ProjectThumbnail.js
+++ components/projects/ProjectThumbnail.js
@@ -1,4 +1,12 @@
+import Image from "next/image";
+import { useState } from "react";
+
+function cn(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
 const ProjectThumbnail = (props) => {
+  const [isLoading, setLoading] = useState(true);
   return (
     <div
       className={`col-span-2 mb-2 xl:mb-0 shadow-xl ${
@@ -6,22 +14,32 @@ const ProjectThumbnail = (props) => {
       }`}
     >
       <div
-        className={`relative cursor-pointer xl:w-[800px]  rounded-md overflow-hidden group flex flex-col`}
+        className={`relative cursor-pointer xl:w-[800px] h-[466px]  rounded-md overflow-hidden group flex flex-col `}
       >
-        <div className="w-full h-9 bg-zinc-800 dark:bg-[#D1D1D1] flex items-center gap-2 px-2 py-1 z-10 relative">
+        <div className="w-full h-9 bg-zinc-800 dark:bg-[#D1D1D1] flex items-center gap-2 px-2 py-1 z-10 ">
           <div className="w-3 h-3 bg-red-500 rounded-full"></div>
           <div className="w-3 h-3 bg-orange-500 rounded-full"></div>
           <div className="w-3 h-3 bg-green-500 rounded-full"></div>
         </div>
         <div
-          className={`group-hover:scale-110 transition duration-1000 ${
+          className={`group-hover:scale-110 h-[430px] transition duration-1000 ${
             props.justify ? "group-hover:-rotate-3" : "group-hover:rotate-3"
           }`}
         >
-          <img
+          <Image
             src={props.image}
             alt=""
-            className="w-full h-full object-contain "
+            width={800}
+            height={430}
+            objectFit="cover"
+            className={cn(
+              "duration-700 ease-in-out",
+              isLoading
+                ? "scale-110 blur-2xl grayscale"
+                : "scale-100 blur-0 grayscale-0"
+            )}
+            // layout="fill"
+            onLoadingComplete={() => setLoading(false)}
           />
         </div>
         <div className="absolute w-full h-full left-0 top-0 bg-black/40 -translate-y-full group-hover:-translate-y-0 transition duration-500"></div>
